0fc0c655db60a787121a09b2f36ecc812ff1090b
Matias Gimenez <matias.gimenez@gmail.com>
2017-07-27 17:16:26 -0300

    add js hot reload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(
   <div>
     <App />
